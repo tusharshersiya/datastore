ebee21745e66e21a6d34e8d36346c5ee9bd68831
tusharshersiya <tusharshersiya07@gmail.com>
2025-01-29 12:15:48 +0530

    final change...

--- src/components/DataStore.jsx
+++ src/components/DataStore.jsx
@@ -2,53 +2,53 @@ import React, { useEffect, useState } from 'react'
 
 function DataStore() {
 
-  const [accno, setAccno] = useState()
-  const [name, setName] = useState("")
+    const [accno, setAccno] = useState()
+    const [name, setName] = useState("")
 
-  function handleAcc(e) {
-    setAccno(e.target.value)
-  }
-
-  function handleName(e) {
-    setName(e.target.value)
-  }
-
-  function handleSubmit() {
-    let mydetail = accno + " " + name;
-    console.log("mydata is", mydata)
-
-    if (localStorage.getItem("user1")) {
-      let previousData = JSON.parse(localStorage.getItem("user1"))
-      let updatedData = previousData.concat(mydetail)
-      localStorage.setItem("user1", JSON.stringify(updatedData))
+    function handleAcc(e) {
+        setAccno(e.target.value)
     }
-    else {
-      localStorage.setItem("user1", JSON.stringify([mydetail]))
+
+    function handleName(e) {
+        setName(e.target.value)
     }
-    setAccno("")
-    setName("")
-  }
+
+    function handleSubmit() {
+        let mydetail = accno + " " + name;
+        console.log("mydetail is", mydetail)
+
+        if (localStorage.getItem("user1")) {
+            let previousData = JSON.parse(localStorage.getItem("user1"))
+            let updatedData = previousData.concat(mydetail)
+            localStorage.setItem("user1", JSON.stringify(updatedData))
+        }
+        else {
+            localStorage.setItem("user1", JSON.stringify([mydetail]))
+        }
+        setAccno("")
+        setName("")
+    }
 
 
-  useEffect(() => {
-    if (!localStorage.getItem("user1"))
-      localStorage.setItem('user1', "[]")
-  }, [])
+    useEffect(() => {
+        if (!localStorage.getItem("user1"))
+            localStorage.setItem('user1', "[]")
+    }, [])
 
 
-  return (
-    <>
+    return (
+        <>
 
-      <div className='shadow w-75 m-auto p-5'></div>
-      <h1 className='m-5 text-center'>DataStore</h1>
-      <input type="text" className='form-control w-75 m-auto mb-3' placeholder='Enter your account no' onChange={handleAcc} value={accno} />
-      <input type="text" className='form-control w-75 m-auto' placeholder='enter your name' onChange={handleName} value={name} />
+            <div className='shadow w-75 m-auto p-5'></div>
+            <h1 className='m-5 text-center'>DataStore</h1>
+            <input type="text" className='form-control w-75 m-auto mb-3' placeholder='Enter your account no' onChange={handleAcc} value={accno} />
+            <input type="text" className='form-control w-75 m-auto' placeholder='enter your name' onChange={handleName} value={name} />
 
-      <button className='btn btn-sm btn-dark px-5 d-block m-auto mt-5' onClick={handleSubmit}>Submit</button>
+            <button className='btn btn-sm btn-dark px-5 d-block m-auto mt-5' onClick={handleSubmit}>Submit</button>
 
 
-    </>
-  )
+        </>
+    )
 }
 
 export default DataStore
